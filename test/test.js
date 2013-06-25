a483b992e48e9184984fd01a6ccf2acfffb6f581
Quinn Slack <qslack@qslack.com>
2013-06-25 00:44:13 -0700

    exports_and_locals test

--- test/test.js
+++ test/test.js
@@ -70,7 +70,7 @@ function check(file, src, ast, r) {
 }
 
 var testCases = [
-  {dir: 'simple', files: ['local_vars.js', 'local_funcs.js', 'node_exports.js', 'reassign_module_exports1.js', 'reassign_module_exports2.js', 'reassign_module_exports3.js', 'conditional_node_export.js', 'complex_node_exports.js']},
+  {dir: 'simple', files: ['local_vars.js', 'local_funcs.js', 'node_exports.js', 'reassign_module_exports1.js', 'reassign_module_exports2.js', 'reassign_module_exports3.js', 'conditional_node_export.js', 'complex_node_exports.js', 'exports_and_locals.js']},
 ];
 
 testCases.forEach(function(testCase) {
---
+++ test/testdata/simple/exports_and_locals.js
@@ -0,0 +1,47 @@
+exports.a = /*DECL*/function(x/*DECLID:'/local:x:'*/, y/*DECLID:'/local:y:'*/) {
+  exports;/*#REF:'exports_and_locals.js'*/ // MAKEITWORK(sqs)
+  module.exports;/*#REF:'exports_and_locals.js'*/ // MAKEITWORK(sqs)
+
+  x;/*REF:'/local:x:'*/
+  y;/*REF:'/local:y:'*/
+  f;/*REF:'/local:f:'*/
+  g;/*REF:'/exports.g'*/
+  exports.g;/*REF:'/exports.g'*/
+  h;/*#REF:'/exports.h'*/
+  exports.h;/*#REF:'/exports.h'*/
+  exports.a;/*REF:'/exports.a'*/
+}/*DECL:{id:'/exports.a'}*/;
+
+function f(w/*DECLID:'/local:w:'*/, z/*DECLID:'/local:z:'*/) {
+  w;/*REF:'/local:w:'*/
+  z;/*REF:'/local:z:'*/
+  f;/*REF:'/local:f:'*/
+  g;/*REF:'/exports.g'*/
+  exports.g;/*REF:'/exports.g'*/
+  h;/*#REF:'/exports.h'*/
+  exports.h;/*#REF:'/exports.h'*/
+  exports.a;/*REF:'/exports.a'*/
+}
+
+function g(q/*DECLID:'/local:q:'*/, r/*DECLID:'/local:r:'*/) {
+  q;/*REF:'/local:q:'*/
+  r;/*REF:'/local:r:'*/
+  f;/*REF:'/local:f:'*/
+  g;/*REF:'/exports.g'*/
+  exports.g;/*REF:'/exports.g'*/
+  h;/*#REF:'/exports.h'*/
+  exports.h;/*#REF:'/exports.h'*/
+  exports.a;/*REF:'/exports.a'*/
+}
+exports.g = g;
+
+var h = function(t/*DECLID:'/local:t:'*/) {
+  t;/*REF:'/local:t:'*/
+  f;/*REF:'/local:f:'*/
+  g;/*REF:'/exports.g'*/
+  exports.g;/*REF:'/exports.g'*/
+  h;/*#REF:'/exports.h'*/
+  exports.h;/*#REF:'/exports.h'*/
+  exports.a;/*REF:'/exports.a'*/
+};
+exports.h = h;
